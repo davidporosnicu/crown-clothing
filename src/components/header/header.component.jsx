1d64a7fde89aa92e7ca26cb9d2f387d28c19c7d5
David Porosnicu <davidporosnicu@Davids-MacBook-Pro.local>
2020-05-27 20:43:27 +0300

    add cart-dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 import { ReactComponent as Logo } from "../../assets/crown.svg";
-
 import "./header.styles.scss";
+
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 const Header = () => (
   <div className="header">
@@ -17,7 +18,9 @@ const Header = () => (
       <Link className="option" to="/contact">
         CONTACT
       </Link>
+      <CartIcon />
     </div>
+    <CartDropdown />
   </div>
 );
 
