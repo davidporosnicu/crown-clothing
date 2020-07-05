785aab1c7443e833afe89a2117f270ff677869ac
David Porosnicu <david94_p@yahoo.com>
2020-07-05 19:27:52 +0300

    add ui form validation and success/fail message

--- client/src/components/contact-form/contact-form.component.jsx
+++ client/src/components/contact-form/contact-form.component.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./contact-form.styles.scss";
 import CustomButton from "../../components/custom-button/custom-button.component";
 import { useFormik } from "formik";
 import axios from "axios";
 
 const ContactForm = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
   const formik = useFormik({
     initialValues: {
       fullName: "",
@@ -15,17 +17,17 @@ const ContactForm = () => {
     validate: values => {
       const errors = {};
       if (!values.fullName) {
-        errors.name = "This field is required";
+        errors.fullName = "Please enter your name";
       }
       if (!values.email) {
-        errors.email = "This field is required";
+        errors.email = "Please enter your email";
       } else if (
         !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
       ) {
         errors.email = "Invalid email address";
       }
       if (!values.message) {
-        errors.message = "This field is required";
+        errors.message = "Please enter your message";
       }
       return errors;
     },
@@ -33,9 +35,18 @@ const ContactForm = () => {
     onSubmit: (values, { setErrors, setSubmitting, resetForm }) => {
       axios
         .post("/api/email", values)
-        .then(res => console.log(res))
-        .catch(err => console.log("err", err));
-      resetForm();
+        .then(res => {
+          setSubmitting(false);
+          setIsSubmitted(true);
+        })
+        .catch(err => {
+          console.log("Error:", err);
+          setSubmitting(false);
+          resetForm();
+          setErrors({
+            messageNotSent: "Something went wrong, please try again later.",
+          });
+        });
     },
   });
 
@@ -47,6 +58,11 @@ const ContactForm = () => {
     errors,
     touched,
   } = formik;
+
+  const fullNameError = errors.fullName && touched.fullName;
+  const emailError = errors.email && touched.email;
+  const messageError = errors.message && touched.message;
+
   return (
     <form onSubmit={handleSubmit} className="contact-form">
       <div className="inputWrapper">
@@ -54,24 +70,55 @@ const ContactForm = () => {
           id="fullName"
           type="text"
           value={fullName}
+          disabled={isSubmitted}
+          className={`${fullNameError && "inputError"}`}
           onChange={handleChange}
         />
         <label htmlFor="fullName">Full Name</label>
+        {fullNameError && <p className="errorMessage">{errors.fullName}</p>}
       </div>
       <div className="inputWrapper">
-        <input id="email" type="email" value={email} onChange={handleChange} />
+        <input
+          id="email"
+          type="email"
+          value={email}
+          disabled={isSubmitted}
+          className={`${emailError && "inputError"}`}
+          onChange={handleChange}
+        />
         <label htmlFor="email">Email Address</label>
+        {emailError && <p className="errorMessage">{errors.email}</p>}
       </div>
       <div className="inputWrapper">
         <input
           id="message"
           type="text"
           value={message}
+          disabled={isSubmitted}
+          className={`${messageError && "inputError"}`}
           onChange={handleChange}
         />
         <label htmlFor="message">Message</label>
+        {messageError && <p className="errorMessage">{errors.message}</p>}
       </div>
-      <CustomButton inverted>SEND MESSAGE</CustomButton>
+
+      {isSubmitted ? (
+        <p style={{ margin: "0" }}>
+          Thank you for your message. We will get back to you as soon as
+          possible.
+        </p>
+      ) : (
+        <>
+          <CustomButton isLoading={isSubmitting} inverted>
+            SEND MESSAGE
+          </CustomButton>
+          {errors.messageNotSent && (
+            <div className="messageNotSent">
+              <p>{errors.messageNotSent}</p>
+            </div>
+          )}
+        </>
+      )}
     </form>
   );
 };
--- client/src/components/custom-button/custom-button.component.jsx
+++ client/src/components/custom-button/custom-button.component.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 import "./custom-button.styles.scss";
 
-const CustomButton = ({ children, inverted, ...props }) => (
-  <button className={`${inverted && "inverted"} custom-button`} {...props}>
+const CustomButton = ({ children, inverted, isLoading, ...props }) => (
+  <button
+    className={`${inverted && "inverted"} custom-button`}
+    disabled={isLoading}
+    {...props}
+  >
     {children}
   </button>
 );
